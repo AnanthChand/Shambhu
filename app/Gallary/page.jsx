0ac2d8fca872dd0722436f1a6aac575d255c1f3d
Ananth Chandra <ananthinindia@gmail.com>
2023-07-03 13:37:53 +0530

    fourth commit

--- app/Gallary/page.jsx
+++ app/Gallary/page.jsx
@@ -25,62 +25,62 @@ import c6 from "..//../public/c6.JPG";
 import { TimelineCards, TitleText, TypingText } from "../../components";
 import Gallary from "@/sections/Gallary";
 
-const Ratha = function () {
-  return (
-    <div>
-      <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-2 ">
-        <div className="-m-1 flex flex-wrap md:-m-2">
-          <div className="flex w-1/2 flex-wrap">
-            <div className="w-1/2 p-1 md:p-2">
-              <Image
-                alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
-                src={ml6}
-              />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image
-                alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
-                src={Image2}
-              />
-            </div>
-            <div className="w-full p-1 md:p-2">
-              <Image
-                alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
-                src={Image3}
-              />
-            </div>
-          </div>
-          <div className="flex w-1/2 flex-wrap">
-            <div className="w-full p-1 md:p-2">
-              <Image
-                alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
-                src={Image4}
-              />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image
-                alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
-                src={Image5}
-              />
-            </div>
-            <div className="w-1/2 p-1 md:p-2">
-              <Image
-                alt="gallery"
-                className="block h-full w-full rounded-lg object-cover object-center"
-                src={Image1}
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+// const Ratha = function () {
+//   return (
+//     <div>
+//       <div className="container mx-auto px-5 py-2 lg:px-32 lg:pt-2 ">
+//         <div className="-m-1 flex flex-wrap md:-m-2">
+//           <div className="flex w-1/2 flex-wrap">
+//             <div className="w-1/2 p-1 md:p-2">
+//               <Image
+//                 alt="gallery"
+//                 className="block h-full w-full rounded-lg object-cover object-center"
+//                 src={ml6}
+//               />
+//             </div>
+//             <div className="w-1/2 p-1 md:p-2">
+//               <Image
+//                 alt="gallery"
+//                 className="block h-full w-full rounded-lg object-cover object-center"
+//                 src={Image2}
+//               />
+//             </div>
+//             <div className="w-full p-1 md:p-2">
+//               <Image
+//                 alt="gallery"
+//                 className="block h-full w-full rounded-lg object-cover object-center"
+//                 src={Image3}
+//               />
+//             </div>
+//           </div>
+//           <div className="flex w-1/2 flex-wrap">
+//             <div className="w-full p-1 md:p-2">
+//               <Image
+//                 alt="gallery"
+//                 className="block h-full w-full rounded-lg object-cover object-center"
+//                 src={Image4}
+//               />
+//             </div>
+//             <div className="w-1/2 p-1 md:p-2">
+//               <Image
+//                 alt="gallery"
+//                 className="block h-full w-full rounded-lg object-cover object-center"
+//                 src={Image5}
+//               />
+//             </div>
+//             <div className="w-1/2 p-1 md:p-2">
+//               <Image
+//                 alt="gallery"
+//                 className="block h-full w-full rounded-lg object-cover object-center"
+//                 src={Image1}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
 
 const MaleLinga = function () {
   return (
@@ -216,12 +216,12 @@ function Page() {
           <Construction />
         </div>
       </div>
-      <div className="flex flex-col object-center items-center mt-10">
+      {/* <div className="flex flex-col object-center items-center mt-10">
         <TypingText title="| The old Ratha" textStyles="text-right" />
         <div>
           <Ratha />
         </div>
-      </div>
+      </div> */}
       <div className="flex flex-col object-center items-center">
         <Link href="/" className="hover:cursor-pointer  ">
           <div className="text-white font-bold text-2xl mt-10 py-4 px-6 bg-[#25618B] rounded-[32px] gap-[12px] items-center object-center">
